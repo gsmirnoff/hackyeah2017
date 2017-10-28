8ea721aa6b6262e940696b52f2ca5717f0c990ff
Greg Smirnoff <gregsm1988@gmail.com>
2017-10-28 16:17:43 +0200

    header

--- vatman-ui/src/app/Header.vue
+++ vatman-ui/src/app/Header.vue
@@ -1,13 +1,45 @@
 <template>
   <header class="header">
-    <p class="header-title">
-      <a href="https://github.com/FountainJS/generator-fountain-webapp" target="_blank" rel="noopener noreferrer">
-        Fountain Generator
-      </a>
-    </p>
-    <p class="header-date">
-      Generated with FountainJS v0.6.0 on Wed Aug 10 2016 10:13:26 GMT+0200 (CEST)
-    </p>
+    <b-navbar toggleable="md" type="dark" variant="info">
+
+      <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+
+      <b-navbar-brand href="#">Fraud detection</b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <b-nav is-nav-bar>
+          <b-nav-item href="#">Rules</b-nav-item>
+          <b-nav-item href="#">Notifications</b-nav-item>
+        </b-nav>
+
+        <!-- Right aligned nav items -->
+        <b-nav is-nav-bar class="ml-auto">
+
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using button-content slot -->
+            <template slot="button-content">
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Signout</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-nav>
+
+      </b-collapse>
+    </b-navbar>
   </header>
 </template>
 
